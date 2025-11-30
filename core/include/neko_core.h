7ba47fb0668d41a3239c99f42bcd3e02656b5155
scarlet <scarryaa@gmail.com>
2025-11-30 00:46:26 -0500

    Project structure/Rust setup

---
+++ core/include/neko_core.h
@@ -0,0 +1,26 @@
+#ifndef NEKO_CORE_H
+#define NEKO_CORE_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Create a new text buffer
+typedef struct NekoBuffer NekoBuffer;
+NekoBuffer *neko_buffer_new(void);
+void neko_buffer_free(NekoBuffer *buffer);
+
+// Insert text at position
+void neko_buffer_insert(NekoBuffer *buffer, size_t pos, const char *text,
+                        size_t len);
+
+// Get buffer content
+const char *neko_buffer_get_text(const NekoBuffer *buffer, size_t *out_len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NEKO_CORE_H
