bf581534b918eb02a6b7a629ee56fb93261b4a9b
scarlet <scarryaa@gmail.com>
2025-12-05 16:58:35 -0500

    Added splitter to main layout

--- desktop/src/features/main_window/main_window.cpp
+++ desktop/src/features/main_window/main_window.cpp
@@ -1,12 +1,29 @@
 #include "main_window.h"
 #include "features/editor/editor_widget.h"
 #include "features/file_explorer/file_explorer_widget.h"
+#include <QSplitter>
+#include <QVBoxLayout>
 #include <neko_core.h>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-  EditorWidget *editorWidget = new EditorWidget();
-  FileExplorerWidget *fileExplorerWidget = new FileExplorerWidget();
-  setCentralWidget(editorWidget);
+  auto *fileExplorerWidget = new FileExplorerWidget();
+  auto *editorWidget = new EditorWidget();
+
+  auto *splitter = new QSplitter(Qt::Horizontal, this);
+  splitter->addWidget(fileExplorerWidget);
+  splitter->addWidget(editorWidget);
+  splitter->setStretchFactor(0, 0);
+  splitter->setStretchFactor(1, 1);
+  splitter->setSizes({250, 600});
+
+  splitter->setHandleWidth(1);
+
+  splitter->setStyleSheet("QSplitter::handle {"
+                          "  background-color: #3c3c3c;"
+                          "  margin: 0px;"
+                          "}");
+
+  setCentralWidget(splitter);
 }
 
 MainWindow::~MainWindow() {}
