4f9a358722c4fe49efb33adbca09deaf8b62bcd8
scarlet <scarryaa@gmail.com>
2025-12-08 12:23:55 -0500

    Added sticky column to cursor movement

--- core/src/text/cursor.rs
+++ core/src/text/cursor.rs
@@ -1,14 +1,21 @@
+use std::cmp::min;
+
 use super::Buffer;
 
 #[derive(Debug, Default, PartialEq, Eq, Clone)]
 pub struct Cursor {
     row: usize,
     column: usize,
+    sticky_column: usize,
 }
 
 impl Cursor {
     pub fn new() -> Self {
-        Self { row: 0, column: 0 }
+        Self {
+            row: 0,
+            column: 0,
+            sticky_column: 0,
+        }
     }
 
     pub fn insert_and_move(&mut self, buffer: &mut Buffer, text: &str) {
@@ -20,8 +27,10 @@ impl Cursor {
             if ch == '\n' {
                 self.row += 1;
                 self.column = 0;
+                self.sticky_column = 0;
             } else {
                 self.column += 1;
+                self.sticky_column = self.column;
             }
         }
     }
@@ -32,6 +41,7 @@ impl Cursor {
 
         self.row += 1;
         self.column = 0;
+        self.sticky_column = 0;
     }
 
     pub fn insert_tab(&mut self, buffer: &mut Buffer) {
@@ -43,6 +53,7 @@ impl Cursor {
         buffer.insert(idx, &spaces);
 
         self.column += tab_width;
+        self.sticky_column += self.column;
     }
 
     pub fn backspace_and_move(&mut self, buffer: &mut Buffer) {
@@ -69,18 +80,22 @@ impl Cursor {
 
         if self.column < buffer.line_len(self.row) {
             self.column += 1;
+            self.sticky_column = self.column;
         } else if self.row + 1 < buffer.line_count() {
             self.row += 1;
             self.column = 0;
+            self.sticky_column = 0;
         }
     }
 
     pub fn move_left(&mut self, buffer: &Buffer) {
         if self.column > 0 {
             self.column -= 1;
+            self.sticky_column = self.column;
         } else if self.row > 0 {
             self.row -= 1;
             self.column = buffer.line_len(self.row);
+            self.sticky_column = buffer.line_len(self.row);
         }
     }
 
@@ -89,12 +104,12 @@ impl Cursor {
             self.row += 1;
 
             let line_len = buffer.line_len(self.row);
-            if self.column > line_len {
-                self.column = line_len;
-            }
+
+            self.column = min(line_len, self.sticky_column);
         } else if self.row + 1 == buffer.line_count() {
             let line_len = buffer.line_len(self.row);
             self.column = line_len;
+            self.sticky_column = line_len;
         }
     }
 
@@ -103,12 +118,11 @@ impl Cursor {
             self.row -= 1;
 
             let line_len = buffer.line_len(self.row);
-            if self.column > line_len {
-                self.column = line_len;
-            }
+            self.column = min(line_len, self.sticky_column);
         } else {
             self.row = 0;
             self.column = 0;
+            self.sticky_column = 0;
         }
     }
 
@@ -116,11 +130,13 @@ impl Cursor {
         let last_line_idx = buffer.line_count() - 1;
         self.row = last_line_idx;
         self.column = buffer.line_len(last_line_idx);
+        self.sticky_column = buffer.line_len(last_line_idx);
     }
 
     pub fn move_to_start(&mut self) {
         self.row = 0;
         self.column = 0;
+        self.sticky_column = 0;
     }
 
     pub fn set_row(&mut self, row: usize) {
@@ -129,6 +145,7 @@ impl Cursor {
 
     pub fn set_col(&mut self, col: usize) {
         self.column = col;
+        self.sticky_column = col;
     }
 
     pub fn get_row(&self) -> usize {
@@ -137,6 +154,10 @@ impl Cursor {
 
     pub fn get_col(&self) -> usize {
         self.column
+    }
+
+    pub fn get_sticky_col(&self) -> usize {
+        self.sticky_column
     }
 
     pub fn get_idx(&self, buffer: &Buffer) -> usize {
